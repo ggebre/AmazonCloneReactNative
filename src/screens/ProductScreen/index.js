cbedebce72a36e2abbdfc0ed86a42b8677ce1141
Getu Gebre <getu.gebre.gg@gmail.com>
2021-06-17 09:40:51 -0500

    options dropdown created using picker

--- src/screens/ProductScreen/index.js
+++ src/screens/ProductScreen/index.js
@@ -1,16 +1,25 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { View, Text} from 'react-native';
 import styles from './styles';
 import product from '../../data/product';
+import {Picker} from '@react-native-picker/picker';
 const ProductScreen = () => {
-
+    const [selectedOption, setSelectedOption] = useState('');
+    
     return (
         <View>
             <Text style={styles.title}> {product.title} </Text>
             {/* Image carousel  */}
 
             {/* option selector */}
-
+            <Picker 
+                selectedValue={selectedOption}
+                onValueChange={(itemValue) => setSelectedOption(itemValue)}
+                >
+                { product.options.map(option => 
+                    <Picker.Item label= {option} value={option} key={option}/>)
+                }
+            </Picker>
             {/* price */}
             <Text style={styles.price}> 
                         from ${product.price} 
@@ -25,6 +34,8 @@ const ProductScreen = () => {
             {/* Quantity selector */}
 
             {/* button */}
+
+            
         </View>
     )
 }
