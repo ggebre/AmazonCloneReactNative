326b17d3364c812736de1b75d1d2d372cb71d75c
Getu Gebre <getu.gebre.gg@gmail.com>
2021-06-18 09:30:29 -0500

    image indicator dots created

--- src/components/ImageCarousol/index.js
+++ src/components/ImageCarousol/index.js
@@ -1,8 +1,10 @@
-import React from 'react';
+import React, { useState, useCallback } from 'react';
 import { View, FlatList, Image, StyleSheet, useWindowDimensions } from 'react-native';
 
 
+
 const ImageCarousol = ({images}) => {
+    const [activeIndex, setActiveIndex] = useState(0)
     const windowWidth = useWindowDimensions().width;
     const renderItem = ({item}) => (
         <Image 
@@ -11,6 +13,12 @@ const ImageCarousol = ({images}) => {
         />
         
     )
+    const onViewableItemsChanged = useCallback(({ viewableItems}) => {
+        if(viewableItems.length > 0){
+            setActiveIndex(viewableItems[0].index )
+        }
+        
+    }, [])
     return (
         <View style={styles.root}>
             <FlatList
@@ -22,9 +30,25 @@ const ImageCarousol = ({images}) => {
                 snapToInterval={windowWidth - 20}
                 snapToAlignment={'center'}
                 decelerationRate={'fast'}
+
+                // to get the viable image index 
+                viewabilityConfig={{
+                    viewAreaCoveragePercentThreshold: 50, 
+                }}
+                onViewableItemsChanged = {onViewableItemsChanged}
                 >
             </FlatList>
-            
+            <View style={styles.dots} >
+                {images.map((image, index) => (
+                <View style={[
+                    styles.dot,
+                    {
+                        backgroundColor: index === activeIndex ? '#c9c9c9' : '#ededed'
+                    }
+                ]} />
+                ))}
+            </View>
+        
         </View>
     )
 }
@@ -37,6 +61,21 @@ const styles = StyleSheet.create({
         margin: 10,
         height: 250,
         resizeMode: 'contain',
-    }
+    },
+    dots: {
+        flexDirection: 'row',
+        justifyContent: 'center'
+    },
+
+    dot: {
+        width: 10,
+        height: 10,
+        borderRadius: 10, 
+        borderWidth: 1, 
+        backgroundColor: '#ededed',
+        borderColor: '#c9c9c9',
+        margin: 5
+    },
+    
 })
 export default ImageCarousol;
