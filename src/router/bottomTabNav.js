538789329c1c5f1667cdd1a3cdeb9073f9fa506c
Getu Gebre <getu.gebre.gg@gmail.com>
2021-06-22 10:29:53 -0500

    tabnav icons created

--- src/router/bottomTabNav.js
+++ src/router/bottomTabNav.js
@@ -2,16 +2,44 @@ import React from 'react';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import HomeScreen from '../screens/homeScreen';
 import ShoppingCartScreen from '../screens/shoppingCartScreen'
-
+import Entypo from 'react-native-vector-icons/Entypo';
 const Tab = createBottomTabNavigator();
 
 const BottomTabNav = () => {
     return (
-        <Tab.Navigator>
-            <Tab.Screen component={HomeScreen} name="Home" />
-            <Tab.Screen component={HomeScreen} name="profile" />
-            <Tab.Screen component={ShoppingCartScreen} name="shoppingCart" />
-            <Tab.Screen component={HomeScreen} name="more" />
+        <Tab.Navigator 
+            tabBarOptions={{
+                showLabel: false, 
+                inactiveBackgroundColor: '#ffbd7e', 
+                activeTintColor: '#e47911'}}>
+            <Tab.Screen 
+                component={HomeScreen} 
+                name="Home" 
+                options = {{
+                    tabBarIcon: ({color}) => (<Entypo name="home" color={color} size={25} />),
+                }}
+            /> 
+            <Tab.Screen 
+                component={HomeScreen} 
+                name="profile"
+                options= {{
+                    tabBarIcon: ({color}) => (<Entypo name="user" color={color} size={25}/>)
+                }} 
+                />
+            <Tab.Screen 
+                component={ShoppingCartScreen} 
+                name="shoppingCart"
+                options= {{
+                    tabBarIcon: ({color}) => (<Entypo name="shopping-cart" color={color} size={25}/>)
+                }}
+                />
+            <Tab.Screen 
+                component={HomeScreen} 
+                name="more" 
+                options= {{
+                    tabBarIcon: ({color}) => (<Entypo name="menu" color={color} size={25}/>)
+                }}
+                />
         </Tab.Navigator>
     )
 }
